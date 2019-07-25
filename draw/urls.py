3a9830d0baff826cae33d350065458ade08120ff
jwu7082 <jwu7082@outlook.com>
2019-07-25 19:24:02 -0400

    added variant URL

--- draw/urls.py
+++ draw/urls.py
@@ -1,10 +1,11 @@
 # chat/urls.py
 from django.conf.urls import url
-
 from . import views
 
+from django.urls import path
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<room_name>[^/]+)/$', views.room, name='room'),
+#     url(r'^(?P<room_name>[^/]+)/$', views.room, name='room'),
+    path('variant/', views.variant, name='variant')
 ]
 
